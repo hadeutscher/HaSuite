cef5e761aebd6a743bb9d70b5ee05d7317a96079
haha01haha01 <haha01haha01@gmail.com>
2015-06-01 00:41:05 +0300

    License headers

--- HaCreator/MapEditor/Instance/ObjectInstanceQuest.cs
+++ HaCreator/MapEditor/Instance/ObjectInstanceQuest.cs
@@ -1,4 +1,10 @@
-﻿using MapleLib.WzLib.WzStructure.Data;
+﻿/* Copyright (C) 2015 haha01haha01
+
+* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+using MapleLib.WzLib.WzStructure.Data;
 using System;
 using System.Collections.Generic;
 using System.Dynamic;
--- HaCreator/MapEditor/SerializationManager.cs
+++ HaCreator/MapEditor/SerializationManager.cs
@@ -1,4 +1,10 @@
-﻿using HaCreator.Collections;
+﻿/* Copyright (C) 2015 haha01haha01
+
+* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+using HaCreator.Collections;
 using HaCreator.MapEditor.Info;
 using HaCreator.MapEditor.Instance;
 using MapleLib.WzLib.WzStructure;
